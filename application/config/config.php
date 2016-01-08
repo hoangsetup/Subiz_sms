2b41ef9e96622cc5bb19ec96afe2abc3803d0c63
Hoangdv <hoangdinh@vietnambiz.com>
2016-01-08 14:29:10 +0700

    change serverip

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.5.207/subiz_sms/';
+$config['base_url'] = 'http://192.168.5.208/subiz_sms/';
 
 /*
 |--------------------------------------------------------------------------
